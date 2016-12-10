1afad1803a93f6501e22d37cf2e5f582c4105dae
Joseph Spens <joseph@spens.us>
2016-12-10 16:38:10 -0500

    Parse meetup CSV and write to JSON

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const fs = require('fs');
+const path = require('path');
+
+const parseMeetup = require('./lib/meetup/parse');
+const writeMeetup = require('./lib/meetup/write');
+const { checkDataSet } = require('./lib/utils');
 
 program
-  .arguments('<file>')
+  .arguments('<input> <output>')
   .option('-s, --source <source>', 'The source data set from which we are consolidating')
   .option('-t, --target <target>', 'The target data set to which we are consolidating')
-  .action(file => {
-    if (typeof program.source === 'undefined') {
-      console.error('No source data set provided.');
+  .action((input, output) => {
+    try {
+      checkDataSet('source', program.source);
+      checkDataSet('target', program.target);
+    } catch (exception) {
+      console.error(exception.message);
       process.exit(1);
     }
-    if (typeof program.target === 'undefined') {
-      console.error('No target data set provided.');
-      process.exit(2);
+
+    console.log(`Consolidating from ${program.source} file ${input} to ${program.target} file ${output}`);
+
+    let sourceProcess;
+    let targetProcess;
+
+    switch (program.source) {
+      case 'meetup':
+        sourceProcess = parseMeetup;
+        break;
+      case 'mailchimp':
+        sourceProcess = parseMeetup;
+        break;
     }
-    console.log(`Consolidating ${file} from ${program.source} to ${program.target}`);
+
+    switch (program.target) {
+      case 'meetup':
+        targetProcess = writeMeetup;
+        break;
+      case 'mailchimp':
+        targetProcess = writeMeetup;
+        break;
+    }
+
+    sourceProcess(input, targetProcess(output));
   })
   .parse(process.argv);
